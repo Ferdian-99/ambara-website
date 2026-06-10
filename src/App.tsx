fe89c7d3d01ec9a955b1ae96a55b116502109048
Ferdian-99 <sensasix68@gmail.com>
2026-06-11 04:10:13 +0700

    Add protected admin and client routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Layout } from "./components";
+import { ProtectedRoute } from "./routes/ProtectedRoute";
+import { AdminClients } from "./pages/admin/AdminClients";
+import { AdminDocuments } from "./pages/admin/AdminDocuments";
+import { AdminHome } from "./pages/admin/AdminHome";
+import { AdminLayout } from "./pages/admin/AdminLayout";
+import { AdminLogin } from "./pages/admin/AdminLogin";
+import { AdminProjectDetail } from "./pages/admin/AdminProjectDetail";
+import { AdminProjectNew } from "./pages/admin/AdminProjectNew";
+import { AdminProjects } from "./pages/admin/AdminProjects";
+import { ClientHome } from "./pages/client/ClientHome";
+import { ClientLayout } from "./pages/client/ClientLayout";
+import { ClientLogin } from "./pages/client/ClientLogin";
+import { ClientProjectDetail } from "./pages/client/ClientProjectDetail";
+import { ClientProjects } from "./pages/client/ClientProjects";
 import { About } from "./pages/About";
 import { Contact } from "./pages/Contact";
 import { Home } from "./pages/Home";
@@ -22,6 +36,39 @@ const router = createBrowserRouter([
       { path: "proses", element: <Process /> },
       { path: "lacak-proyek", element: <Tracking /> },
       { path: "kontak", element: <Contact /> },
+    ],
+  },
+  { path: "/admin/login", element: <AdminLogin /> },
+  {
+    path: "/admin",
+    element: <ProtectedRoute allowedRoles={["super_admin", "content_manager", "project_manager", "sales"]} loginPath="/admin/login" />,
+    children: [
+      {
+        element: <AdminLayout />,
+        children: [
+          { index: true, element: <AdminHome /> },
+          { path: "projects", element: <AdminProjects /> },
+          { path: "projects/new", element: <AdminProjectNew /> },
+          { path: "projects/:id", element: <AdminProjectDetail /> },
+          { path: "clients", element: <AdminClients /> },
+          { path: "documents", element: <AdminDocuments /> },
+        ],
+      },
+    ],
+  },
+  { path: "/client/login", element: <ClientLogin /> },
+  {
+    path: "/client",
+    element: <ProtectedRoute allowedRoles={["client"]} loginPath="/client/login" />,
+    children: [
+      {
+        element: <ClientLayout />,
+        children: [
+          { index: true, element: <ClientHome /> },
+          { path: "projects", element: <ClientProjects /> },
+          { path: "projects/:id", element: <ClientProjectDetail /> },
+        ],
+      },
     ],
   },
 ]);
